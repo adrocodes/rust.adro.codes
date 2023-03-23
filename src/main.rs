96f1054d6c5eee87b3005b2c17aebfe5c9c938b9
adrocodes <adrocodes@users.noreply.github.com>
2023-03-23 19:53:13 +1100

    feat: collect md files from site directory

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod filesystem;
+
+use std::{
+    io,
+    path::{Path, PathBuf},
+};
+
+use filesystem::collect_path_with_ext;
+
+fn collect_site_markdown_files() -> io::Result<Vec<PathBuf>> {
+    Ok(collect_path_with_ext(
+        &Path::new("./site"),
+        &vec![".mdx", ".md"],
+    )?)
+}
+
+fn main() -> io::Result<()> {
+    let _markdown_paths = collect_site_markdown_files()?;
+
+    Ok(())
 }
